4dbf616d91f97e10e8a116f2fdc64e3d4937059a
suryawanshishreyas <suryawanshishreyas@gmail.com>
2023-12-23 14:49:11 +0530

    Navbar changes

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+import ui from './module2.mjs';
+
+console.log(ui);
+// console.log(c);
+// console.log(a);
+// console.log(b);
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+const a = "Shreyas";
+const b ="Rohan";
+const c ="Shardul";
+const d ="Suryawanshi";
+
+export default d;
