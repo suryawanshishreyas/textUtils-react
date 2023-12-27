918473029dfae8a01e3e64ca13069632a0440a2f
suryawanshishreyas <suryawanshishreyas@gmail.com>
2023-12-27 19:42:43 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
 
 
 function App() {
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#0b0442';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-      <Navbar title ="Web Page" aboutText = "About Text Utils" />
+      <Navbar title ="Web Page" aboutText = "About" mode={mode} toggleMode={toggleMode}/>
       <div className='container'>
-        <TextForm heading ='Enter the text below to analyze below'/>
+        <TextForm heading ='Enter the text below to analyze below' mode={mode}/>
+        <About/>
       </div>
-      
+
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,26 +25,25 @@ export default function TextForm(props) {
   return (
     <>
     <div>
-      <div className="container mb-3 my-3">
+      <div className="container mb-3 my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+        <input type="email" style={{backgroundColor:props.mode==='dark'?'grey':'white'}} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
       </div>
-      <div className="container mb-3 my-3">
+      <div className="container mb-3 my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
-        <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange} value={text} ></textarea>
+        <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8" onChange={handleOnChange} value={text} ></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick} >Convert to Lower Case</button>
       <button className="btn btn-primary mx-2" onClick={handleReverse} >Reverse the Text</button>
     </div>
-    <div className="container my-3">
+    <div className="container mb-3 my-3" style={{color:props.mode==='dark'?'white':'black'}}>
       <h2>Your Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{Math.floor(0.008 * text.split(" ").length)} Minutes Read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p style={{color:props.mode ==='dark'?'Red':'Grey'}}>{text.length>0?text:"Enter Something to Preview"}</p>
     </div>
     </>
-    
   )
 }
